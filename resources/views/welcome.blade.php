0580b0d7270b8b6ab6e364ffe31fb04bfe82848e
Rhaima96 <mohamed.rhaima96@gmail.com>
2021-01-22 20:44:53 +0100

    correction footer dans la page Acceuil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,51 +86,7 @@
             </div>
         </div>
     </div>
-    <div class="footer-dark">
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-md-3 item">
-                        <h2 style="font-family: Conv_Lora;margin-left: -1px;"><strong>Site Menu</strong></h2>
-                        <ul>
-                            <li style="font-family: 'Ink Free';"><a href="#" style="font-size: 1.5rem;">Home</a></li>
-                            <li><a href="#" style="font-family: 'Ink Free';font-size: 1.5rem;">About us</a></li>
-                            <li><a href="#" style="font-family: 'Ink Free';font-size: 1.5rem;">Get in Touch</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-6 col-md-3 item">
-                        <h2 style="font-family: Conv_Lora;margin-left: -1px;"><strong>Contacts</strong></h2>
-                        <ul>
-                            <li><i class="fa fa-map-marker" style="font-size: 28px;color: #00bed6;"></i>&nbsp; King Abudullaziz Rd, Riyadh,<br>Saudi Arabia</li>
-                            <li><i class="fa fa-envelope" style="font-size: 20px;color: #00bed6;"></i>&nbsp; inquiries@mndoob.co</li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6 item text">
-                        <h2 style="font-family: Conv_Lora;margin-left: -1px;"><strong>Follow us</strong></h2>
-                        <div class="row">
-                            <div class="col d-xl-flex flex-column justify-content-xl-center align-items-xl-center">
-                                <h3 style="font-family: 'Ink Free';font-size: 1.5rem;">For Customer</h3>
-                                <div class="row">
-                                    <div class="col-lg-6"><a href="#"><img src="assets/img/hero-playstore.png" width="110px"></a></div>
-                                    <div class="col-lg-6"><a href="#"><img src="assets/img/hero-appstore.png" width="110px"></a></div>
-                                </div>
-                            </div>
-                            <div class="col d-xl-flex flex-column justify-content-xl-center align-items-xl-center">
-                                <h3 style="font-family: 'Ink Free';font-size: 1.5rem;">For&nbsp;Provider</h3>
-                                <div class="row">
-                                    <div class="col-lg-6"><a href="#"><img src="assets/img/hero-playstore.png" width="110px"></a></div>
-                                    <div class="col-lg-6"><a href="#"><img src="assets/img/hero-appstore.png" width="110px"></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col item social" style="border-color: rgb(255,255,255);"><a href="#" style="background: #ffffff;"><i class="fa fa-facebook" style="color: #00bed6;"></i></a><a href="#" style="background: #ffffff;"><i class="icon ion-social-twitter" style="color: #00bed6;"></i></a><a href="#" style="background: #ffffff;"><i class="fa fa-instagram" style="color: #00bed6;"></i></a><a href="#" style="background: #ffffff;"><i class="fa fa-youtube-play" style="color: #00bed6;"></i></a></div>
-                </div>
-                <hr style="border-color: rgb(108,118,124);width: 70%;">
-                <div class="col d-flex justify-content-between"><span>© 2020 MNDOOB Inc.</span><span><a class="text-decoration-none" href="#" style="color: rgb(251,253,255);font-family: 'Ink Free';font-size: 1.2rem;">Privacy Policy&nbsp; &nbsp; &nbsp;&nbsp;</a><a class="text-decoration-none" href="#" style="color: rgb(251,253,255);font-family: 'Ink Free';font-size: 1.2rem;"><span style="text-decoration: underline;">Terms of use</span></a></span></div>
-            </div>
-        </footer>
-    </div>
+
     @include('partials.footer')
 
 
